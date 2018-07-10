5d893921be43ca647d5bde2e9ac18d114dba7f6c
amoffitt7 <32561146+amoffitt7@users.noreply.github.com>
2018-07-10 15:23:33 -0700

    Removed unnecessary file creation
    Updated so that we don't create a file for every pairs flow graph but instead just the adjacency matrix.

--- ReplicationCutAlgorithm.java
+++ ReplicationCutAlgorithm.java
@@ -69,6 +69,10 @@ public class ReplicationCutAlgorithm {
     
     // function to create replication graph for a specified pair
     public void CreateReplicationGraph(int s, int t) { 
+    	int newVertices = numberOfVertices * 2 + 2;
+    	int[][] adjMatrix = new int[newVertices + 1][newVertices + 1];
+    	
+    	
         try {
             reportpw = new PrintWriter(new FileWriter(reportFileName, true));
             //System.out.println("In single pair. Finding the cut between " + s + " and " + t + ":");
@@ -77,22 +81,12 @@ public class ReplicationCutAlgorithm {
         } catch (IOException e) {
             System.err.println("Error");
         }
+        
+        
         try {
-            
-
             BufferedReader br = new BufferedReader(new FileReader(file));
 
-            String[] fileNameParts = file.getAbsolutePath().split("\\.");
-            String repGraphFileName = fileNameParts[0] + "_" + s + "_" + t + ".txt";
-            //BufferedWriter bw = new BufferedWriter(new FileWriter(repGraphFileName));
-            PrintWriter pw = new PrintWriter(new FileWriter(repGraphFileName));
-
             Integer myInf = Integer.MAX_VALUE;
-
-            // this is the number of vertices in the replication graph; 
-            // all the original vertices duplicated plus super source and super sink
-            int newVertices = numberOfVertices * 2 + 2;
-            pw.format("%5d\r\n", newVertices);
 
             // get the header out of the way
             if ((br.readLine()) == null) {
@@ -112,9 +106,9 @@ public class ReplicationCutAlgorithm {
                 // creates new vertices using old ones and reverses direction
                 int newFirstVertex = secondVertex + numberOfVertices; 
                 int newSecondVertex = firstVertex + numberOfVertices;
-
-                pw.format("%5d   %5d   %5d \r\n", firstVertex, secondVertex, edgeWeight); // add the original edge
-                pw.format("%5d   %5d   %5d \r\n", newFirstVertex, newSecondVertex, edgeWeight); // add the new edge
+                
+                adjMatrix[firstVertex][secondVertex] = edgeWeight;
+                adjMatrix[newFirstVertex][newSecondVertex] = edgeWeight;
             }
 
             int supersource = newVertices - 1; // the supersource will take the second to last index
@@ -124,65 +118,28 @@ public class ReplicationCutAlgorithm {
             int tPrime = t + numberOfVertices;
 
             // add the edges from supersource (s*) to sources (s and s')
-            pw.format("%5d   %5d   %5d \r\n", supersource, s, myInf); 
-            pw.format("%5d   %5d   %5d \r\n", supersource, sPrime, myInf); 
+            adjMatrix[supersource][s] = myInf;
+            adjMatrix[supersource][sPrime] = myInf;
+           
 
             // add the edges from sinks (t and t') to supersink 
-            pw.format("%5d   %5d   %5d \r\n", t, supersink, myInf); 
-            pw.format("%5d   %5d   %5d \r\n", tPrime, supersink, myInf); 
+            adjMatrix[t][supersink] = myInf;
+            adjMatrix[tPrime][supersink] = myInf; 
 
             // for all vertices that are not source and sink, add infinite paths from vertices to their primes
             for (int i = 1; i <= numberOfVertices; i++) {
                 if (i != s && i != t) {
                     int iPrime = i + numberOfVertices;
-                    pw.format("%5d   %5d   %5d \r\n", i, iPrime, myInf); 
+                    adjMatrix[i][iPrime] = myInf; 
                 }
             }
 
-            pw.close();
             br.close();
-            createAdjacencyMatrix(repGraphFileName, supersource, supersink, newVertices);
+            runFlowAlgorithm(adjMatrix, supersource, supersink, newVertices);
         }
         catch (IOException e) {
             System.err.println("Error reading file");
         }
-    }
-    
-    
-    
-    public void createAdjacencyMatrix(String filename, int s, int t, int numVertices) {
-    	//System.out.println("In matrix chunk");
-    	File file = new File(filename);
-    	int[][] adjMatrix = new int[numVertices + 1][numVertices + 1];
-    	    	
-    	try {
-    		BufferedReader br = new BufferedReader(new FileReader(file));
-    		
-    		// get the first line out of the way
-            if ((br.readLine()) == null) {
-            	System.err.println("First Line ERROR");
-            }
-
-            // Grab edge by edge
-            String nextLine;
-            while ((nextLine = br.readLine()) != null) {
-                nextLine = nextLine.trim();
-                String[] entries = nextLine.split("\\s+"); // splits into 3 numbers
-                
-                int firstVertex = Integer.parseInt(entries[0]);
-                int secondVertex = Integer.parseInt(entries[1]);
-                int edgeWeight = Integer.parseInt(entries[2]);
-
-                adjMatrix[firstVertex][secondVertex] = edgeWeight;
-            } 		
-
-            br.close();
-
-    	} catch(IOException e) {
-    		System.err.println("Read ERROR");
-    	}
-    	
-    	runFlowAlgorithm(adjMatrix, s, t, numVertices);
     }
     
     
