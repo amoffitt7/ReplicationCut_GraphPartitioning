dcb1ac8ea880c23961838888089750035ca5195f
amoffitt7 <32561146+amoffitt7@users.noreply.github.com>
2018-07-09 18:54:32 -0700

    Added code to test many files at once
    User just has to specify a folder that contains other folders that contain graph files

--- Run_Test.java
+++ Run_Test.java
@@ -4,20 +4,31 @@
  * and then call "java Run_Test filename.txt"
  */
 
+import java.io.*;
+import java.util.*;
+
+
 public class Run_Test {
 
-	public static void main(String[] args) {
-		//PULL FILES FROM STORED LOCATION
-        String fileName = args[0];
-        System.out.println("Argument string = " + fileName);
-
-        ReplicationCutAlgorithm repCut = new ReplicationCutAlgorithm(fileName);
-        repCut.findDistinctNumberOfMinCuts();
-
+	public static void main(String[] args) {	
+		final File mainFolder = new File(args[0]);
+		for(final File folderEntry: mainFolder.listFiles()) {
+			for(final File fileEntry: listFilesForFolder(folderEntry)) {
+				System.out.println("Argument string = " + fileEntry);
+				ReplicationCutAlgorithm repCut = new ReplicationCutAlgorithm(fileEntry.getAbsolutePath());
+		        repCut.findDistinctNumberOfMinCuts();
+			}
+		}
+	}	
+	
+	
+	public static File[] listFilesForFolder(final File folder) {
+		List<File> files = new ArrayList<File>();
+	    for (final File fileEntry : folder.listFiles()) {
+	        files.add(fileEntry);
+	    }
+	    
+	    File filesArray[] = new File[files.size()];
+	    return files.toArray(filesArray);
 	}
 }
-
-
-
-
-
